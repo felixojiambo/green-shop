33c51ad6364bd204686e331ffa0885e659898194
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 11:50:44 -0700

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ConfirmationService } from 'primeng/api';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { customInterceptor } from './shared/interceptors/custom.interceptor';
+import { errorInterceptor } from './shared/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideToastr(),
+    provideHttpClient(withInterceptors([customInterceptor, errorInterceptor])),
+    ConfirmationService, provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,70 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './pages/admin/cart/cart.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { CategoryProductsComponent } from './pages/website/category-products/category-products.component';
+import { CheckoutComponent } from './pages/website/checkout/checkout.component';
+import { CustomerOrdersComponent } from './pages/website/customer-orders/customer-orders.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+import { WebProductsComponent } from './pages/website/web-products/web-products.component';
+import { authGuard } from './shared/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: '',
+    redirectTo: 'AllProducts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LandingComponent,
+    children: [
+      {
+        path: 'AllProducts',
+        component: WebProductsComponent,
+        title: 'All-Products'
+      },
+      {
+        path: 'products/:id',
+        component: CategoryProductsComponent
+      },
+      {
+        path: 'cart',
+        component: CartComponent
+      }
+    ]
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    canActivate: [authGuard],
+    title: 'Checkout'
+  },
+  {
+    path: 'order-history',
+    component: CustomerOrdersComponent,
+    canActivate: [authGuard],
+    title: 'Your Orders'
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'products',
+        component: ProductsComponent,
+        title: 'Products'
+      },
+      {
+        path: 'category',
+        component: CategoriesComponent,
+        title: 'Category'
+      }
+    ]
+  }];
